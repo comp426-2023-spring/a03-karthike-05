08823584951554f97a7b318dce5059220414b0a4
karthike-05 <karthike@email.unc.edu>
2023-04-28 04:37:52 -0400

    gamelogic

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -29,32 +29,28 @@ catch (error){
 }
 
 
-function(help)
+function help()
 {
-    console.log(
-        'Usage: node-rps [SHOT]
-        Play Rock Paper Scissors (RPS)
-        
-          -h, --help      display this help message and exit
-          -r, --rules     display the rules and exit
-        
-        Examples:
-          node-rps        Return JSON with single player RPS result.
-                          e.g. {"player":"rock"}
-          node-rps rock   Return JSON with results for RPS played against a simulated opponent.
-                          e.g {"player":"rock","opponent":"scissors","result":"win"}'
-    );
+    console.log('Usage: node-rps [SHOT]' +
+    'Play Rock Paper Scissors (RPS)' +
+    '' +
+    '  -h, --help      display this help message and exit' +
+    '  -r, --rules     display the rules and exit ' +
+    '' +
+    'Examples:'+
+    '  node-rps        Return JSON with single player RPS result.'+
+    '                  e.g. {"player":"rock"}' + 
+     ' node-rps rock   Return JSON with results for RPS played against a simulated opponent.'+
+     '                 e.g {"player":"rock","opponent":"scissors","result":"win"}');
     process.exit(0);
 }
 
-function(rules)
+function rules()
 {
-    console.log(
-        'Rules for Rock Paper Scissors:
-
-        - Scissors CUTS Paper
-        - Paper COVERS Rock
-        - Rock CRUSHES Scissors'
-    );
+    console.log('Rules for Rock Paper Scissors:' + 
+    '' +
+    '- Scissors CUTS Paper' +
+    '- Paper COVERS Rock' +
+    '- Rock CRUSHES Scissors');
     process.exit(0);
 }
